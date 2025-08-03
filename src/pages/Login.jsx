01c1a68a4ef30f601f490db38960ad19f31cca15
Sundus Asif <sundusasif634@gmail.com>
2025-08-04 02:07:46 +0500

    ff

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -89,7 +89,7 @@ const LoginPage = () => {
                <img 
                  src="/loginlogo.jpg" 
                  alt="Tax Nexus Logo" 
-                 className="w-64 h-64 object-cover rounded-lg shadow-lg"
+                 className="w-64 h-64 object-contain rounded-lg shadow-lg bg-white"
                />
              </div>
              <p className="text-gray-600 text-lg font-medium">Tax Consulting Platform</p>
